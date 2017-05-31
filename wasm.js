c6905201bc9ac4431e5a18b0b2d4dfa3cb2d8d2b
oscarcs <nxtrafalgar@yahoo.co.nz>
2017-05-31 23:58:10 +1200

    Add additional functions in wrapper JS.

--- wasm.js
+++ wasm.js
@@ -3,9 +3,22 @@ const args = process.argv;
 const buf = fs.readFileSync(`${args[2]}/${args[2]}.wasm`);
 
 var array = new Uint8Array(buf); 
+var memory = new WebAssembly.Memory({ initial : 1 });
 
 var imports = {js: {
-    print_int: (x) => console.log(x)
+    mem: memory,
+    
+    print_int: (x) => process.stdout.write(x.toString()),
+
+    print_string: function(offset, length) {
+        // Retrieve bytes from the linear memory
+        var bytes = new Uint8Array(memory.buffer, offset, length);
+        
+        // Convert to 'UCS-2'
+        var string = String.fromCharCode.apply(null, new Uint16Array(bytes));
+        
+        process.stdout.write(string);
+    }
 }};
 
 WebAssembly.instantiate(array, imports).then(wasm =>
